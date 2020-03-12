5e03ebde6757912d83b7243386748556461130fa
Junior Marques <junioralcant111@gmail.com>
2020-03-12 15:16:41 -0300

    Importacao do controller de fieis para o arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const route = express.Router();
 
-route.get("/", (req, res) => {
-  return res.send("OK");
-});
+const FieisController = require("./controllers/FieisController");
+
+route.get("/fieis", FieisController.index);
+route.post("/fieis", FieisController.store);
+route.get("/fieis/:id", FieisController.show);
+route.put("/fieis/:id", FieisController.update);
+route.delete("/fieis/:id", FieisController.delete);
 
 module.exports = route;
