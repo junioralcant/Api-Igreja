dee85ba10fc8a77dd79e94d16b426687da6c056e
Junior Marques <junioralcant111@gmail.com>
2020-03-12 15:14:55 -0300

    Criacao do controller de fieis

---
+++ src/controllers/FieisController.js
@@ -0,0 +1,43 @@
+const Fieis = require("../models/Fieis");
+
+class FieisController {
+  async index(req, res) {
+    const fieis = await Fieis.paginate();
+
+    return res.json(fieis);
+  }
+
+  async store(req, res) {
+    const fieu = await Fieis.create(req.body);
+
+    return res.json(fieu);
+  }
+
+  async show(req, res) {
+    const fieu = await Fieis.findById(req.params.id);
+
+    return res.json(fieu);
+  }
+
+  async update(req, res) {
+    const fieu = await Fieis.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+
+    return res.json(fieu);
+  }
+
+  async store(req, res) {
+    const fieu = await Fieis.create(req.body);
+
+    return res.json(fieu);
+  }
+
+  async delete(req, res) {
+    await Fieis.findByIdAndDelete(req.params.id);
+
+    return res.send();
+  }
+}
+
+module.exports = new FieisController();
