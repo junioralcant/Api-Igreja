ab4ca46237a6e9a1ffb72ee923fbdc8445a4a3b8
Junior Marques <junioralcant111@gmail.com>
2020-03-12 14:22:12 -0300

    Conexao com banco de dados e instalacao do nodemon

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/igreja", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.get("/", (req, res) => {
   return res.send("OK");
