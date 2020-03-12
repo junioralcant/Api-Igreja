3a3ddb693e34e4a2cdf6f872fc36fbc1c72142d8
Junior Marques <junioralcant111@gmail.com>
2020-03-12 15:59:17 -0300

    Importacao do controller de dizimo para arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@ const express = require("express");
 const route = express.Router();
 
 const FieisController = require("./controllers/FieisController");
+const DizimoController = require("./controllers/DizimoController");
 
 route.get("/fieis", FieisController.index);
 route.post("/fieis", FieisController.store);
 route.get("/fieis/:id", FieisController.show);
 route.put("/fieis/:id", FieisController.update);
 route.delete("/fieis/:id", FieisController.delete);
+
+route.get("/dizimos", DizimoController.index);
+route.post("/dizimos", DizimoController.store);
+route.get("/dizimos/:id", DizimoController.show);
+route.put("/dizimos/:id", DizimoController.update);
+route.delete("/dizimos/:id", DizimoController.delete);
 
 module.exports = route;
